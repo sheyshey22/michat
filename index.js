8533c14267a0faa468a9213ba8277bcdab18f3bb
sheyla <s.vilca.neira@gmail.com>
2016-05-26 13:33:27 -0500

    otro

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static('public'));
+
+
+io.on('connection', function(socket){
+	console.log('Tenemos una nueva victima!!');
+
+	socket.on('chat', function(_msg){
+		io.emit('nuevo_mensaje', _msg);
+	});
+
+});
+
+
+
+
+
+
+
+
+
+http.listen(8080, function () {
+	console.log('Muy bien!, eres un crack, sabes trabajar con node JS');
+});
